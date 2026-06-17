3252e6bfb58254bf56b18b1a14fa5822c3c3038e
silvarc141 <silvarc141@users.noreply.github.com>
2026-06-17 08:30:51 +0200

    fix termux setup

--- termux.sh
+++ termux.sh
@@ -1,5 +1,5 @@
 # Run with something like this:
-# bash <(curl -s https://raw.githubusercontent.com/silvarc141/notes-nu/main/termux-setup)
+# . $(curl -s https://raw.githubusercontent.com/silvarc141/notes-nu/main/termux.sh)
 
 # Other optional setup:
 # termux-change-repo
@@ -7,8 +7,9 @@
 # chsh nu
 
 pkg install nushell git git-crypt ripgrep 
-exec nu -c '
+nu -c '
 let lib = http get https://raw.githubusercontent.com/silvarc141/notes-nu/main/lib.nu
 let dir = $nu.user-autoload-dirs | first
+mkdir $dir
 $lib o> ($dir | path join "notes.nu")
 '
